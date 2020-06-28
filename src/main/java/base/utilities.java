3bc3c1714f71117c7056747c3a6285b6c869352d
venkatunr <42603518+venkatunr@users.noreply.github.com>
2020-06-28 20:01:04 +0530

    Updated code for click action

--- src/main/java/base/utilities.java
+++ src/main/java/base/utilities.java
@@ -2,10 +2,12 @@ package base;
 
 public class utilities {
 	
-	public static void clickfunction() {
+	public static void clickfunction(WebElement element) {
 		/*
 		 * // Code wil be here // and
 		 */	
+		JavascriptExecutor jse=(JavascriptExecutor)driver;
+		jse.excuteScript("arguments[0].click();" element)
 		}
 
 }
